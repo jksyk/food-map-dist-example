6c51698778023069df35a781de2ce0397e1175da
jksyk <jksyk@naver.com>
2023-02-05 21:22:06 +0900

    axios url변경

--- front/axios-script.js
+++ front/axios-script.js
@@ -42,7 +42,7 @@ async function getDataSet(category) {
 
   const dataSet = await axios({
     method: "get", // http method
-    url: `http://localhost:3000/restaurants?category=${qs}`,
+    url: `http://3.38.76.218:3000/restaurants?category=${qs}`,
     headers: {}, // packet header
     data: {}, // packet body
   });
